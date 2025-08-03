960a57dc86594186711696185f64347a0a0d0510
rashid0056100 <rashidkhandharala@gmail.com>
2025-08-03 19:45:33 +0500

    function file upated

--- function.py
+++ function.py
@@ -54,14 +54,10 @@ print(add(34,43))
 a=[1,2,3,4]
 square=list(map(lambda x:x**2,a))
 print(square)
-
-
+# new 
 nums = [1, 2, 3, 4,5,6,7,8]
 evens = list(filter(lambda x: x % 2 == 0, nums))
 print(evens)
-
-
-
 data = [(2, 'b'), (1, 'a'), (3, 'c')]
 sort_data=sorted(data,key=lambda x:x[1])
 numbers = [x[0] for x in sort_data]
